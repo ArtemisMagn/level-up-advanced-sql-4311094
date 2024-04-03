189e9d764bead29068cee0fe54825a03a4415895
ArtemisMagn <165646921+ArtemisMagn@users.noreply.github.com>
2024-04-03 12:33:11 +0000

    Added 01-03

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -10,3 +10,17 @@ LEFT JOIN sales  ON emp.employeeId = sales.employeeId
 WHERE emp.title = 'Sales Person' AND sales.salesAmount IS NULL
 ;
 --01-03
+SELECT cst.firstName, cst.lastName, cst.email, sls.salesAmount, sls.soldDate
+FROM customer cst 
+JOIN sales sls ON cst.customerId = sls.customerId
+UNION
+SELECT cst.firstName, cst.lastName, cst.email, sls.salesAmount, sls.soldDate
+FROM customer cst 
+LEFT JOIN sales sls ON cst.customerId = sls.customerId
+WHERE sls.salesId IS NULL
+UNION
+SELECT cst.firstName, cst.lastName, cst.email, sls.salesAmount, sls.soldDate
+FROM sales sls
+LEFT JOIN customer cst ON cst.customerId = sls.customerId
+WHERE cst.customerId IS NULL
+;
